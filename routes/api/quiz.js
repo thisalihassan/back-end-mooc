f3eacfc52848c24079da37b78f04cf42e33c13c1
alihassan <24819103+thisalihassan@users.noreply.github.com>
2020-07-16 21:41:46 +0500

    new fix

--- routes/api/quiz.js
+++ routes/api/quiz.js
@@ -134,7 +134,8 @@ router.post("/findquiz", [auth], async (req, res) => {
 
         if (solvedQioz) {
           for (let i = 0; i < quiz.length; i++) {
-            if (solvedQioz.find((x) => x.quiz !== quiz[i]._id)) {
+            const match = solvedQioz.find((x) => x.quiz === quiz[i]._id);
+            if (!match) {
               arr.push(quiz[i]);
             }
           }
